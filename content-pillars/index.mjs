4fc537d8114de126e064ab2e116e9b979d511559
UP <up@MacBookPro.home>
2026-08-23 14:31:23 +0200

    Blog, golfreizen, golfshow-toepassingen, diensten, referenties, partners en nieuwe fotos

--- content-pillars/index.mjs
+++
@@ -1,5 +0,0 @@
-/* Pillar-pagina's: diensten van Mark Reynolds (golfshows, golftrips). */
-import golfshows from './golfshows.mjs';
-import golftrips from './golftrips.mjs';
-
-export const PILLARS = [golfshows, golftrips];
